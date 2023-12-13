2ea8037a1a39fd010b5e73fca587e7ac2f2c648d
braun07 <joaovitorbraun6@gmail.com>
2023-12-13 18:15:21 -0300

    HOTFIX REGISTER3

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './main.scss';
 import { Login } from './Login/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { RedirectComponent } from './RedirectComponent';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        {/* <Route path="/" element={<RedirectComponent />} /> */}
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   </React.StrictMode>,
